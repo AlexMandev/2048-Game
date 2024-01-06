86adc3f1f0c217c1c03dda64d7454a8eb3fe1833
AlexMandev <alexmandev2004@gmail.com>
2024-01-06 14:27:19 +0200

    Gameplay logic and fixed input

--- 2048_Game/2048_Game/2048_Game.cpp
+++ 2048_Game/2048_Game/2048_Game.cpp
@@ -1,88 +1,209 @@
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
 
-bool isValidSize(int n, int minSize, int maxSize) {
-    return (n >= minSize && n <= maxSize);
+constexpr int MIN_MATRIX_SIZE = 4;
+constexpr int MAX_MATRIX_SIZE = 10;
+constexpr int MAX_INPUT_STRING_LEN = 100;
+
+void clearConsole() {
+    system("CLS");
 }
-bool isValidMenuOption(int n, int menuSize) {
-    return (n >= 1 && n <= menuSize);
+
+// STR/INPUT/HELPER FUNCTIONS
+
+unsigned strLen(char* str) {
+    if (!str)
+        return 0;
+
+    unsigned length = 0;
+
+    while (*str) {
+        length++;
+        str++;
+    }
+    return length;
+}
+
+char* readStr() {
+    char* input = new char[MAX_INPUT_STRING_LEN];
+    std::cin.getline(input, MAX_INPUT_STRING_LEN);
+    return input;
+}
+
+
+bool isDigit(char ch) {
+    return (ch >= '0' && ch <= '9');
+}
+
+int charToIndex(char ch) {
+    if (ch >= '0' && ch <= '9')
+        return ch - '0';
+    return 0;
+}
+
+int convertToNum(const char* str) {
+    if (!str)
+        return 0;
+
+    int number = 0;
+
+    while (*str) {
+        
+
+        if (!isDigit(*str)) {
+            return 0;
+        }
+
+        int index = charToIndex(*str);
+
+        (number *= 10) += index;
+        str++;
+    }
+
+    return number;
+}
+
+int readNum() {
+    char* input = readStr();
+    int num = convertToNum(input);
+    delete[] input;
+    return num;
+}
+
+unsigned countDigits(unsigned n) {
+    if (n == 0)
+        return 1;
+    
+    int count = 0;
+    while (n != 0) {
+        count++;
+        n /= 10;
+    }
+    return count;
+}
+
+bool isValidSize(int n) {
+    return (n >= MIN_MATRIX_SIZE && n <= MAX_MATRIX_SIZE);
+}
+bool isValidMenuOption(int n) {
+    return (n >= 1 && n <= 3);
+}
+bool isValidMove(char* move) {
+    if (!move || (strLen(move)!= 1))
+        return false;
+    
+    return (*move == 'w' || *move == 'a' || *move == 's' || *move == 'd');
 }
 
 void printMenu() {
-    std::cout << "1. Start game" << std::endl;
-    std::cout << "2. Leaderboard" << std::endl;
-    std::cout << "3. Exit" << std::endl;
+    std::cout << "1. Start Game\n";
+    std::cout << "2. Leaderboard\n";
+    std::cout << "3. Exit\n";
 }
 
-void errorCommands() {
-    constexpr int CHARACTERS_TO_IGNORE = 128;
+int readMenuChoice() {
+    std::cout << "Choose your option(1-3): ";
+    int choice = readNum();
 
-    std::cin.clear();
-    std::cin.ignore(CHARACTERS_TO_IGNORE, '\n');
-    std::cout << "Invalid option!" << std::endl;
+    while (!(choice >= 1 && choice <= 3)) {
+        clearConsole();
+        printMenu();
+        std::cout << "Invalid option!\n";
+        std::cout << "Choose a new option(1-3): ";
+        choice = readNum();
+    }
+
+    return choice;
 }
 
+int readMatrixSize() {
+    std::cout << "Choose a field size (" << MIN_MATRIX_SIZE << "-" << MAX_MATRIX_SIZE << "): ";
+    int size = readNum();
 
-void inputMenuChoiceMessage(int menuSize) {
-    std::cout << "Choose an option (1-" << menuSize << "): ";
-}
-
-void inputMatrixSizeMessage(int minSize, int maxSize) {
-    std::cout << "Choose a field size (" << minSize << "-" << maxSize << "): ";
-}
-
-char* getPlayerNickname() {
-    constexpr unsigned MAX_LENGTH = 64;
-    char nickname[MAX_LENGTH];
-
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    std::cout << "Enter your nickname (max length " << MAX_LENGTH << " characters): ";
-    std::cin.getline(nickname, MAX_LENGTH);
-
-    return nickname;
-}
-
-int getMatrixSize() {
-    constexpr int MIN_SIZE = 4;
-    constexpr int MAX_SIZE = 10;
-
-    inputMatrixSizeMessage(MIN_SIZE, MAX_SIZE);
-    int size;
-    std::cin >> size;
-
-    while (!isValidSize(size, MIN_SIZE, MAX_SIZE)) {
-        errorCommands();
-        inputMatrixSizeMessage(MIN_SIZE, MAX_SIZE);
-        std::cin >> size;
+    while (!isValidSize(size)) {
+        clearConsole();
+        std::cout << "Invalid size! Size should be between " << MIN_MATRIX_SIZE << " and " << MAX_MATRIX_SIZE << "!\n";
+        std::cout << "Enter a new size: ";
+        size = readNum();
     }
 
     return size;
 }
 
-int getMenuChoice() {
-    constexpr int MENU_SIZE = 3;
+// MATRIX FUNCTIONS
 
-    inputMenuChoiceMessage(MENU_SIZE);
-    int choice;
-    std::cin >> choice;
-    
-    while (!isValidMenuOption(choice, MENU_SIZE)) {
-        errorCommands();
-        inputMenuChoiceMessage(MENU_SIZE);
-        std::cin >> choice;
+unsigned calculateScore(unsigned** mtx, int size) {
+    if (!mtx)
+        return 0;
+
+    unsigned score = 0;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            score += mtx[i][j];
+        }
     }
-    return choice;
+    return score;
 }
 
-int** createMatrix(int size) {
-    int** mtx = new int* [size];
+unsigned findMaxInMatrix(unsigned** mtx, int size) {
+    if (!mtx)
+        return 0;
+    unsigned max = 0;
 
     for (int i = 0; i < size; i++)
-        mtx[i] = new int[size] {0};
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if (mtx[i][j] > max)
+                max = mtx[i][j];
+        }
+    }
+    return max;
+}
 
+void printMatrix(unsigned** mtx, int size, unsigned& score) {
+    clearConsole();
+
+    unsigned max = findMaxInMatrix(mtx, size);
+    int digitsOfMax = countDigits(max);
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            int whitespace = 0;
+            if (j != 0)
+                whitespace = 3;
+
+            std::cout << std::setw(digitsOfMax + whitespace) << mtx[i][j]; 
+        }
+        std::cout << "\n\n";
+    }
+
+    score = calculateScore(mtx, size);
+    std::cout << "Score: " << score << "\n\n";
+}
+
+unsigned** createMatrix(int size) {
+    unsigned** mtx = new unsigned* [size];
+
+    for (int i = 0; i < size; i++) {
+        mtx[i] = new unsigned[size];
+
+        for (int j = 0; j < size; j++)
+        {
+            mtx[i][j] = 0;
+        }
+    }
+    
     return mtx;
 }
 
-void deleteMatrix(int** mtx, int size) {
+void deleteMatrix(unsigned** mtx, int size) {
     if (!mtx)
         return;
 
@@ -91,33 +212,345 @@ void deleteMatrix(int** mtx, int size) {
     delete[] mtx;
 }
 
-void startGame() {
-    char* playerNickname = getPlayerNickname();
+bool areValidIndices(int x, int y, int size) {
+    return (x >= 0 && x < size) && (y >= 0 && y < size);
+}
 
-    std::cout << playerNickname;
+bool containsEmptySpace(unsigned** mtx, int size);
 
-    int size = getMatrixSize();
+void generateRandomInMatrix(unsigned** mtx, int size, unsigned score) {
+    if (!mtx || !containsEmptySpace(mtx, size))
+        return;
 
-    int** mtx = createMatrix(size);
+    constexpr unsigned LOWER_SCORE_GENERATOR = 2048;
+    constexpr unsigned HIGHER_SCORE_GENERATOR = 8192;
+
+    srand(time(0));
+
+    int row = rand() % size;
+    int col = rand() % size;
+
+    while (mtx[row][col] != 0) {
+        row = rand() % size;
+        col = rand() % size;
+    }
+
+    unsigned values[12] = { 2, 2, 2, 2, 2, 2, 4, 4, 4, 4, 8, 8 };
+
+    int indexBorder = 0;
+    if (score < LOWER_SCORE_GENERATOR) {
+        indexBorder = 7;
+    }
+    else if (score < HIGHER_SCORE_GENERATOR) {
+        indexBorder = 9;
+    }
+    else {
+        indexBorder = 12;
+    }
+
+    int index = rand() % indexBorder;
+    unsigned value = values[index];
+
+    mtx[row][col] = value;
+}
+
+bool hasEqualAdjacentNumbers(unsigned** mtx, int size) {
+    if (!mtx)
+        return false;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            if ((areValidIndices(i + 1, j, size) && mtx[i + 1][j] == mtx[i][j])
+                || (areValidIndices(i, j + 1, size) && mtx[i][j + 1] == mtx[i][j])) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool containsEmptySpace(unsigned** mtx, int size) {
+    if (!mtx)
+        return false;
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++)
+        {
+            if (!mtx[i][j])
+                return true;
+        }
+    }
+    return false;
+}
+
+bool existsPossibleMove(unsigned** mtx, int size) {
+    if (!mtx)
+        return false;
+
+
+
+    return (containsEmptySpace(mtx, size) || hasEqualAdjacentNumbers(mtx, size));
+}
+
+void moveLeft(unsigned** mtx, int size, bool& movedOrSummed) {
+    if (!mtx)
+        return;
+
+    for (int i = 0; i < size; i++) {
+        int pos = 0;
+        for (int j = 0; j < size; ++j) {
+            if (mtx[i][j] != 0) {
+                mtx[i][pos++] = mtx[i][j];
+
+                if (pos - 1 != j) {
+                    movedOrSummed = true;
+                }
+            }
+        }
+
+        for (int j = pos; j < size; j++) {
+            mtx[i][j] = 0;
+        }
+    }
+}
+
+void moveRight(unsigned** mtx, int size, bool& movedOrSummed) {
+    if (!mtx)
+        return;
+
+    for (int i = 0; i < size; i++)
+    {
+        int pos = size - 1;
+        for (int j = size-1; j >= 0; j--)
+        {
+            if (mtx[i][j] != 0) {
+                mtx[i][pos--] = mtx[i][j];
+
+                if (pos + 1 != j)
+                    movedOrSummed = true;
+            }
+        }
+
+        for (int j = pos; j >= 0; j--) {
+            mtx[i][j] = 0;
+        }
+    }
     
+}
+void moveUp(unsigned** mtx, int size, bool& movedOrSummed) {
+    for (int j = 0; j < size; j++)
+    {
+        int pos = 0;
+        for (int i = 0; i < size; i++)
+        {
+            if (mtx[i][j]) {
+                mtx[pos++][j] = mtx[i][j];
 
+                if (pos - 1 != i)
+                    movedOrSummed = true;
+            }
+        }
+        
+        for (int i = pos; i < size; i++)
+        {
+            mtx[i][j] = 0;
+        }
+    }
+}
+void moveDown(unsigned** mtx, int size, bool& movedOrSummed) {
+    for (int j = 0; j < size; j++)
+    {
+        int pos = size - 1;
+        for (int i = size-1; i >= 0; i--)
+        {
+            if (mtx[i][j] != 0) {
+                mtx[pos--][j] = mtx[i][j];
 
+                if (pos + 1 != i)
+                    movedOrSummed = true;
+            }
+        }
+
+        for (int i = pos; i >= 0; i--)
+        {
+            mtx[i][j] = 0;
+        }
+    }
+}
+
+void sumElementsLeft(unsigned** mtx, int size, bool& movedOrSummed) {
+    if (!mtx)
+        return;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size - 1; j++)
+        {
+            if (mtx[i][j] == mtx[i][j + 1] && mtx[i][j] != 0) {
+                mtx[i][j] *= 2;
+                mtx[i][j + 1] = 0;
+                movedOrSummed = true;
+            }
+        }
+    }
+}
+void sumElementsRight(unsigned** mtx, int size, bool& movedOrSummed) {
+    if (!mtx)
+        return;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = size-1; j > 0; j--)
+        {
+            if (mtx[i][j] == mtx[i][j - 1] && mtx[i][j] != 0) {
+                mtx[i][j] *= 2;
+                mtx[i][j - 1] = 0;
+                movedOrSummed = true;
+            }
+        }
+    }
+}
+void sumElementsUp(unsigned** mtx, int size, bool& movedOrSummed) {
+    if (!mtx)
+        return;
+
+    for (int j = 0; j < size; j++)
+    {
+        for (int i = 0; i < size-1; i++)
+        {
+            if (mtx[i][j] == mtx[i+1][j] && mtx[i][j] != 0) {
+                mtx[i][j] *= 2;
+                mtx[i+1][j] = 0;
+                movedOrSummed = true;
+            }
+        }
+    }
+}
+void sumElementsDown(unsigned** mtx, int size, bool& movedOrSummed) {
+    if (!mtx)
+        return;
+    
+    for (int j = 0; j < size; j++) {
+        for (int i = size - 1; i > 0; i--)
+        {
+            if (mtx[i][j] == mtx[i - 1][j] && mtx[i][j] != 0) {
+                mtx[i][j] *= 2;
+                mtx[i-1][j] = 0;
+                movedOrSummed = true;
+            }
+
+        }
+    }
+}
+
+void executeLeft(unsigned** mtx, int size, bool& moved) {
+    moveLeft(mtx, size, moved);
+
+    sumElementsLeft(mtx, size, moved);
+
+    moveLeft(mtx, size, moved);
+}
+void executeRight(unsigned** mtx, int size, bool& moved) {
+    moveRight(mtx, size, moved);
+
+    sumElementsRight(mtx, size, moved);
+
+    moveRight(mtx, size, moved);
+}
+void executeUp(unsigned** mtx, int size, bool& moved) {
+    moveUp(mtx, size, moved);
+
+    sumElementsUp(mtx, size, moved);
+
+    moveUp(mtx, size, moved);
+}
+void executeDown(unsigned** mtx, int size, bool& moved) {
+    moveDown(mtx, size, moved);
+
+    sumElementsDown(mtx, size, moved);
+
+    moveDown(mtx, size, moved);
+}
+
+void executeMove(unsigned** mtx, int size, char direction, bool& moved) {
+    switch (direction) {
+        case 'w':
+            executeUp(mtx, size, moved);
+            break;
+        case 'a':
+            executeLeft(mtx, size, moved);
+            break;
+        case 's':
+            executeDown(mtx, size, moved);
+            break;
+        case 'd':
+            executeRight(mtx, size, moved);
+            break;
+        default:
+            return;
+    }
+}
+
+void startGame() {
+    clearConsole();
+    std::cout << "Enter your name: ";
+    char* playerName = readStr();
+
+    int size = readMatrixSize();
+
+    unsigned** mtx = createMatrix(size);
+
+    generateRandomInMatrix(mtx, size, 0);
+    unsigned score = 0;
+    printMatrix(mtx, size, score);
+
+    while (existsPossibleMove(mtx, size)) {
+        std::cout << "Enter a move(w,a,s,d): ";
+        char* move = readStr();
+
+        while(!isValidMove(move)) {
+            delete[] move;
+            clearConsole();
+            printMatrix(mtx, size, score);
+            std::cout << "Invalid move!\nChoose a new move(w,a,s,d): ";
+            move = readStr();
+        }
+
+        bool moved = false;
+
+        executeMove(mtx, size, move[0], moved);
+
+        clearConsole();
+
+        if (moved)
+            generateRandomInMatrix(mtx, size, score);
+
+        printMatrix(mtx, size, score);
+
+        delete[] move;
+    }
+
+    delete[] playerName;
     deleteMatrix(mtx, size);
+    // TO DO
 }
 
 void leaderboard() {
-
+    clearConsole();
+    // TO DO
 }
 
 void exit() {
-
+    // TO DO
 }
 
 
 void runGame() {
     printMenu();
     
-    int menuChoice = getMenuChoice();
+    int menuChoice = readMenuChoice();
     
     switch (menuChoice) {
         case 1:
@@ -127,10 +560,13 @@ void runGame() {
             break;
         case 3:
             break;
+        default:
+            break;
     }
 }
 
 int main()
 {
     runGame();
+    return 0;
 }
