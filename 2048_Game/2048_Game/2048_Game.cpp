4a5291b7ba24c9fc3144d188cd06fb8998436590
AlexMandev <alexmandev2004@gmail.com>
2024-01-21 22:22:52 +0200

    Leaderboard functions implementation

--- 2048_Game/2048_Game/2048_Game.cpp
+++ 2048_Game/2048_Game/2048_Game.cpp
@@ -1,17 +1,34 @@
+/**
+*
+* Solution to course project # 4
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2023/2024
+*
+* @author Aleksandar Mandev
+* @idnumber 5MI0600297
+* @compiler Visual Studio
+*
+* Code for 2048
+*
+*/
+
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
+
+using namespace std;
 
 constexpr int MIN_MATRIX_SIZE = 4;
 constexpr int MAX_MATRIX_SIZE = 10;
-constexpr int MAX_INPUT_STRING_LEN = 100;
+constexpr int MAX_INPUT_STRING_LEN = 101;
+constexpr int MAX_LEADERBOARD_PLAYERS = 5;
 
 void clearConsole() {
     system("CLS");
 }
-
-// STR/INPUT/HELPER FUNCTIONS
 
 unsigned strLen(char* str) {
     if (!str)
@@ -26,9 +43,40 @@ unsigned strLen(char* str) {
     return length;
 }
 
+void strCpy(const char* source, char* dest) {
+    if (!source || !dest)
+        return;
+
+    while (*source)
+    {
+        *dest = *source;
+        source++;
+        dest++;
+    }
+    *dest = '\0';
+}
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+void swap(char* a, char* b) {
+    int lengthOfA = strLen(a);
+
+    char* temp = new char[lengthOfA + 1];
+    strCpy(a, temp);
+    strCpy(b, a);
+    strCpy(temp, b);
+
+    delete[] temp;
+}
+
+// IO FUNCTIONS
+
 char* readStr() {
     char* input = new char[MAX_INPUT_STRING_LEN];
-    std::cin.getline(input, MAX_INPUT_STRING_LEN);
+    cin.getline(input, MAX_INPUT_STRING_LEN);
     return input;
 }
 
@@ -41,6 +89,11 @@ int charToIndex(char ch) {
     if (ch >= '0' && ch <= '9')
         return ch - '0';
     return 0;
+}
+char digitToChar(int n) {
+    if (n >= 0 && n <= 9)
+        return '0' + n;
+    return '\0';
 }
 
 int convertToNum(const char* str) {
@@ -98,20 +151,20 @@ bool isValidMove(char* move) {
 }
 
 void printMenu() {
-    std::cout << "1. Start Game\n";
-    std::cout << "2. Leaderboard\n";
-    std::cout << "3. Exit\n";
+    cout << "1. Start Game\n";
+    cout << "2. Leaderboard\n";
+    cout << "3. Exit\n";
 }
 
 int readMenuChoice() {
-    std::cout << "Choose your option(1-3): ";
+    cout << "Choose your option(1-3): ";
     int choice = readNum();
 
     while (!(choice >= 1 && choice <= 3)) {
         clearConsole();
         printMenu();
-        std::cout << "Invalid option!\n";
-        std::cout << "Choose a new option(1-3): ";
+        cout << "Invalid option!\n";
+        cout << "Choose a new option(1-3): ";
         choice = readNum();
     }
 
@@ -119,18 +172,134 @@ int readMenuChoice() {
 }
 
 int readMatrixSize() {
-    std::cout << "Choose a field size (" << MIN_MATRIX_SIZE << "-" << MAX_MATRIX_SIZE << "): ";
+    cout << "Choose a field size (" << MIN_MATRIX_SIZE << "-" << MAX_MATRIX_SIZE << "): ";
     int size = readNum();
 
     while (!isValidSize(size)) {
         clearConsole();
-        std::cout << "Invalid size! Size should be between " << MIN_MATRIX_SIZE << " and " << MAX_MATRIX_SIZE << "!\n";
-        std::cout << "Enter a new size: ";
+        cout << "Invalid size! Size should be between " << MIN_MATRIX_SIZE << " and " << MAX_MATRIX_SIZE << "!\n";
+        cout << "Enter a new size: ";
         size = readNum();
     }
 
     return size;
 }
+
+// LEADERBOARD FUNCTIONS
+
+void sortLeaderboard(char nicknames[][MAX_INPUT_STRING_LEN], unsigned* scores, int indexAdded) {
+    for (int i = indexAdded - 1; i >= 0; i--)
+    {
+        if (scores[i] < scores[indexAdded]) {
+            swap(scores[i], scores[indexAdded]);
+            swap(nicknames[i], nicknames[indexAdded]);
+            indexAdded = i;
+        }
+    }
+}
+
+char* generateFileName(int mtxSize) {
+    int fileLength = countDigits(mtxSize) + 5;
+
+    char* name = new char[fileLength];
+
+    int index = 0;
+
+    if (mtxSize < 10) {
+        name[index++] = digitToChar(mtxSize);
+    }
+    else {
+        name[index++] = '1';
+        name[index++] = '0';
+    }
+
+    name[index++] = '.';
+    name[index++] = 't';
+    name[index++] = 'x';
+    name[index++] = 't';
+    name[index++] = '\0';
+
+    return name;
+}
+
+void updateHighscores(char* nickname, unsigned score, int size) {
+    char nicknames[MAX_LEADERBOARD_PLAYERS + 1][MAX_INPUT_STRING_LEN];
+    unsigned scores[MAX_LEADERBOARD_PLAYERS + 1];
+
+    strCpy(nickname, nicknames[MAX_LEADERBOARD_PLAYERS]);
+    scores[MAX_LEADERBOARD_PLAYERS] = score;
+
+    unsigned playersInFile = 0;
+
+    char* fileName = generateFileName(size);
+
+    ifstream ifs(fileName);
+
+    if (ifs.is_open())
+    {
+
+        for (int i = 0; i < MAX_LEADERBOARD_PLAYERS; i++) // read the leaderboard
+        {
+            ifs.getline(nicknames[i], MAX_INPUT_STRING_LEN, '\n');
+            if (nicknames[i][0] != '\0') {
+                ifs >> scores[i];
+                ifs.ignore(MAX_INPUT_STRING_LEN, '\n');
+                playersInFile++;
+            }
+            else {
+                playersInFile = i;
+                break;
+            }
+        }
+
+        for (int i = playersInFile; i < MAX_LEADERBOARD_PLAYERS; i++)
+            scores[i] = 0;
+
+        bool added = false;
+        int addedIndex = 0;
+
+        for (int i = MAX_LEADERBOARD_PLAYERS - 1; i >= 0; i--) // add the new score, if needed
+        {
+            if (scores[i] < score) {
+                strCpy(nickname, nicknames[i]);
+                scores[i] = score;
+                added = true;
+                addedIndex = i;
+                break;
+            }
+        }
+
+        if (added) {
+            sortLeaderboard(nicknames, scores, addedIndex); // sort if new score is added
+            if (playersInFile != MAX_LEADERBOARD_PLAYERS)
+                playersInFile++;
+        }
+
+        ifs.clear();
+        ifs.close();
+
+        ofstream ofs(fileName);
+        for (int i = 0; i < playersInFile; i++)
+        {
+            ofs << nicknames[i] << '\n' << scores[i] << '\n';
+        }
+
+        ofs.clear();
+        ofs.close();
+    }
+    else {
+        ofstream ofs;
+        ofs.open(fileName);
+        
+        ofs << nickname << '\n' << score << '\n';
+
+        ofs.clear();
+        ofs.close();
+    }
+
+    delete[] fileName;
+}
+
 
 // MATRIX FUNCTIONS
 
@@ -179,13 +348,13 @@ void printMatrix(unsigned** mtx, int size, unsigned& score) {
             if (j != 0)
                 whitespace = 3;
 
-            std::cout << std::setw(digitsOfMax + whitespace) << mtx[i][j]; 
+            cout << setw(digitsOfMax + whitespace) << mtx[i][j]; 
         }
-        std::cout << "\n\n";
+        cout << "\n\n";
     }
 
     score = calculateScore(mtx, size);
-    std::cout << "Score: " << score << "\n\n";
+    cout << "Score: " << score << "\n\n";
 }
 
 unsigned** createMatrix(int size) {
@@ -204,9 +373,6 @@ unsigned** createMatrix(int size) {
 }
 
 void deleteMatrix(unsigned** mtx, int size) {
-    if (!mtx)
-        return;
-
     for (int i = 0; i < size; i++)
         delete[] mtx[i];
     delete[] mtx;
@@ -216,14 +382,26 @@ bool areValidIndices(int x, int y, int size) {
     return (x >= 0 && x < size) && (y >= 0 && y < size);
 }
 
-bool containsEmptySpace(unsigned** mtx, int size);
+bool containsEmptySpace(unsigned** mtx, int size) {
+    if (!mtx)
+        return false;
+
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++)
+        {
+            if (!mtx[i][j])
+                return true;
+        }
+    }
+    return false;
+}
 
 void generateRandomInMatrix(unsigned** mtx, int size, unsigned score) {
     if (!mtx || !containsEmptySpace(mtx, size))
         return;
 
-    constexpr unsigned LOWER_SCORE_GENERATOR = 2048;
-    constexpr unsigned HIGHER_SCORE_GENERATOR = 8192;
+    constexpr unsigned LOWER_SCORE_GENERATOR = 1500;
+    constexpr unsigned HIGHER_SCORE_GENERATOR = 3000;
 
     srand(time(0));
 
@@ -235,7 +413,7 @@ void generateRandomInMatrix(unsigned** mtx, int size, unsigned score) {
         col = rand() % size;
     }
 
-    unsigned values[12] = { 2, 2, 2, 2, 2, 2, 4, 4, 4, 4, 8, 8 };
+    unsigned values[12] = { 2, 2, 2, 2, 2, 2, 4, 4, 4, 4, 4, 8 };
 
     int indexBorder = 0;
     if (score < LOWER_SCORE_GENERATOR) {
@@ -271,25 +449,9 @@ bool hasEqualAdjacentNumbers(unsigned** mtx, int size) {
     return false;
 }
 
-bool containsEmptySpace(unsigned** mtx, int size) {
-    if (!mtx)
-        return false;
-
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; j++)
-        {
-            if (!mtx[i][j])
-                return true;
-        }
-    }
-    return false;
-}
-
 bool existsPossibleMove(unsigned** mtx, int size) {
     if (!mtx)
         return false;
-
-
 
     return (containsEmptySpace(mtx, size) || hasEqualAdjacentNumbers(mtx, size));
 }
@@ -300,7 +462,7 @@ void moveLeft(unsigned** mtx, int size, bool& movedOrSummed) {
 
     for (int i = 0; i < size; i++) {
         int pos = 0;
-        for (int j = 0; j < size; ++j) {
+        for (int j = 0; j < size; j++) {
             if (mtx[i][j] != 0) {
                 mtx[i][pos++] = mtx[i][j];
 
@@ -367,7 +529,7 @@ void moveDown(unsigned** mtx, int size, bool& movedOrSummed) {
         {
             if (mtx[i][j] != 0) {
                 mtx[pos--][j] = mtx[i][j];
-
+                
                 if (pos + 1 != i)
                     movedOrSummed = true;
             }
@@ -495,7 +657,7 @@ void executeMove(unsigned** mtx, int size, char direction, bool& moved) {
 
 void startGame() {
     clearConsole();
-    std::cout << "Enter your name: ";
+    cout << "Enter your name: ";
     char* playerName = readStr();
 
     int size = readMatrixSize();
@@ -506,15 +668,16 @@ void startGame() {
     unsigned score = 0;
     printMatrix(mtx, size, score);
 
+
     while (existsPossibleMove(mtx, size)) {
-        std::cout << "Enter a move(w,a,s,d): ";
+        cout << "Enter a move(w,a,s,d): ";
         char* move = readStr();
 
         while(!isValidMove(move)) {
             delete[] move;
             clearConsole();
             printMatrix(mtx, size, score);
-            std::cout << "Invalid move!\nChoose a new move(w,a,s,d): ";
+            cout << "Invalid move!\nChoose a new move(w,a,s,d): ";
             move = readStr();
         }
 
@@ -524,7 +687,7 @@ void startGame() {
 
         clearConsole();
 
-        if (moved)
+        if(moved)
             generateRandomInMatrix(mtx, size, score);
 
         printMatrix(mtx, size, score);
@@ -534,39 +697,76 @@ void startGame() {
 
     delete[] playerName;
     deleteMatrix(mtx, size);
-    // TO DO
 }
 
 void leaderboard() {
     clearConsole();
-    // TO DO
-}
 
-void exit() {
-    // TO DO
-}
+    cout << "Choose field size for the leaderboard(" << MIN_MATRIX_SIZE << '-' << MAX_MATRIX_SIZE << "): ";
+    int size = readNum();
 
+    while (size < MIN_MATRIX_SIZE || size > MAX_MATRIX_SIZE) {
+        clearConsole();
+        cout << "Invalid option!\n";
+        cout << "Choose a new field size for the leaderboard(" << MIN_MATRIX_SIZE << '-' << MAX_MATRIX_SIZE << "): ";
+        size = readNum();
+    }
+
+    clearConsole();
+
+    char* fileName = generateFileName(size);
+    
+    ifstream ifs(fileName);
+
+    if (!ifs.is_open()) {
+        cout << "Leaderboard for " << size << "x" << size << " is still empty.\n\n";
+        delete[] fileName;
+        return;
+    }
+
+    char nickname[MAX_INPUT_STRING_LEN];
+    int score;
+
+    for (int i = 0; i < MAX_LEADERBOARD_PLAYERS; i++)
+    {
+        ifs.getline(nickname, MAX_INPUT_STRING_LEN, '\n');
+        if (nickname[0] != '\0') {
+            ifs >> score;
+            ifs.ignore(MAX_INPUT_STRING_LEN, '\n');
+            cout << i + 1 << ". " << nickname << " - " << score << "\n";
+        }
+    }
+    cout << '\n';
+
+    ifs.clear();
+    ifs.close();
+    delete[] fileName;
+}
 
 void runGame() {
-    printMenu();
-    
-    int menuChoice = readMenuChoice();
-    
-    switch (menuChoice) {
+    bool running = true;
+
+    while (running) {
+        printMenu();
+        int menuChoice = readMenuChoice();
+
+        switch (menuChoice) {
         case 1:
             startGame();
             break;
         case 2:
+            leaderboard();
             break;
         case 3:
+            running = false;
             break;
         default:
             break;
+        }
     }
 }
 
 int main()
 {
     runGame();
-    return 0;
 }
